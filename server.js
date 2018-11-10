a767e63f8b43ca048630dd2bb7f65c8b80c451b7
Aarsh Patil <patilaarsh@gmail.com>
2018-11-10 09:51:36 -0500

    Changed port 2

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
+const xport = process.env.PORT || 5000
 
 app.get('/first', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(12345, function () {
+var server = app.listen(xport, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", host, xport)
 })
