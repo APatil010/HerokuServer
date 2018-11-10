3a856e17224715bd0fad66c9c3f9474e003a965f
Aarsh Patil <patilaarsh@gmail.com>
2018-11-10 10:53:23 -0500

    Added body parser

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 const xport = process.env.PORT || 5000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/first', function (req, res) {
    res.send('Hello World');
