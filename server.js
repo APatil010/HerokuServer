3e15649ac470e836fa0bc8919dbec2df1de256ce
Aarsh Patil <patilaarsh@gmail.com>
2018-11-10 10:40:19 -0500

    Added method upload

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/first', function (req, res) {
    res.send('Hello World');
 })
 
+app.post('/upload', function (req,res) {
+    var temp = req.body;
+    res.send(temp);
+
+    console.log(temp);
+})
+
 var server = app.listen(xport, function () {
    var host = server.address().address
    var port = server.address().port
